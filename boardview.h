0f995cad8a6f53d6cb0f82e585490ca4c9ee680a
Robert Ikanov <robert.ikanov@gmail.com>
2018-04-21 07:59:45 +0200

    [S] multiple UI support

---
+++ boardview.h
@@ -0,0 +1,56 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2018  <copyright holder> <email>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef BOARDVIEW_H
+#define BOARDVIEW_H
+#include<vector>
+#include"../Engine/enums.h"
+#include"../View2D/sdl_dep.h"
+class BoardView
+{
+protected:
+    int handles[20][20] = {};
+    void createWindow();
+    
+    SDL_Event event;
+    SDL_Point mouse;
+    SDL_Point mouse_act;
+    
+    
+    void selectGroup();
+    void selectJump();
+    
+    virtual void showTileCursor() =0;
+    virtual void showSelected() =0;
+    virtual void redrawBoard() =0;
+    virtual void moveTileCursorTo(const int& row, const int& col) =0;
+    virtual bool convertToTilePosition(int& x, int& y) const =0;
+public:
+    std::vector<SDL_Point> marked;
+    std::vector<SDL_Point> selected;
+  
+    virtual int createPieces(const Ally & A, const int& col, const int& row, const char*) =0;
+    virtual void moveCharacterTo(const int& handle, const int& col, const int& row) =0;
+    virtual void moveSelection() =0;
+    virtual void show() =0;
+    void /*std::vector<int> &&*/ select();
+};
+
+#endif // BOARDVIEW_H
